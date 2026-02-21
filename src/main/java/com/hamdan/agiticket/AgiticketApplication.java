9580c89fed9978faf6bdf30add6e48f6009584d3
Gabriel Hamdan <gabriel_hamdan@hotmail.com>
2026-02-21 11:30:53 -0300

    first commit

---
+++ src/main/java/com/hamdan/agiticket/AgiticketApplication.java
@@ -0,0 +1,13 @@
+package com.hamdan.agiticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgiticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgiticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamdan/agiticket/AgiticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamdan.agiticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
